0e78d71645369e7ed55c2b4166dd24669889cbfb
Mohammad Mikhi <mohammad012010@hotmail.com>
2022-08-01 13:09:32 +0300

    Added messages model and it takes data from form

--- theater/models.py
+++ theater/models.py
@@ -1,3 +1,4 @@
+from email import message
 from django.db import models
 
 # Create your models here.
@@ -6,5 +7,13 @@ class Movies(models.Model):
     name= models.CharField(max_length=100)
     posterURL= models.TextField()
     description=models.TextField(default="")
+    def __str__(self):
+        return self.name
+
+class Messages(models.Model):
+    name= models.CharField(max_length=100)
+    email=models.EmailField()
+    message=models.TextField()
+
     def __str__(self):
         return self.name
--- theater/views.py
+++ theater/views.py
@@ -1,10 +1,22 @@
+from email import message
+import email
+from urllib import request
 from django.shortcuts import render
 
-from theater.models import Movies
+from theater.models import Movies,Messages
 
 # Create your views here.
-def home(response):
-    return render(response, 'home.html',{"movies": Movies.objects.all() })
+def home(request):
+    if request.method== 'POST':
+        if 'messageSubmit' in request.POST: 
+            name1=request.POST['name']
+            message1=request.POST['message']
+            email1=request.POST['email']
+            newMessage= Messages(name=name1,message=message1,email=email1)
+            newMessage.save()
+       # elif 'bookingSubmit' in request.POST:
+            
+    return render(request, 'home.html',{"movies": Movies.objects.all() })
 def bookings(response):
     pass
 def aboutus(response):
