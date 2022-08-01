0d4bc80fd1ceb752aa189109e465bf184a2d9e6d
Mohammad Mikhi <mohammad012010@hotmail.com>
2022-08-01 12:08:44 +0300

    test commit

--- theater/models.py
+++ theater/models.py
@@ -5,6 +5,6 @@ from django.db import models
 class Movies(models.Model):
     name= models.CharField(max_length=100)
     posterURL= models.TextField()
-    description=models.TextField(default="")
+    description=models.TextField(default="desc")
     def __str__(self):
         return self.name
