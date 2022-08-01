5775d97eea1225350f4e9374905c40345061f69f
Mohammad Mikhi <mohammad012010@hotmail.com>
2022-08-01 12:09:34 +0300

    commit

--- theater/models.py
+++ theater/models.py
@@ -5,6 +5,6 @@ from django.db import models
 class Movies(models.Model):
     name= models.CharField(max_length=100)
     posterURL= models.TextField()
-    description=models.TextField(default="desc")
+    description=models.TextField(default="")
     def __str__(self):
         return self.name
